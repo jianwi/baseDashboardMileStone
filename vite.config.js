b9386fb4a78a99ca6d6882d0f905760cdaac7304
dujianjun <v@cm321.cn>
2024-05-23 21:28:39 +0800

    dist

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { semiTheming } from "vite-plugin-semi-theming";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: './',
-    plugins: [react()],
+    plugins: [react(),
+        semiTheming({
+            theme: "@semi-bot/semi-theme-feishu-dashboard",
+        }),],
     server: {
         host: '0.0.0.0',
     }
